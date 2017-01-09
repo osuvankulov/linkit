ed19b10ce24b024e551a053b969106bd5ce25585
Otabek Suvankulov <osuvankulov@gmail.com>
2017-01-08 21:11:28 -0800

    change led to light

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var m = require('mraa');
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var myLed = new m.Gpio(44);
+var myLed = new m.Gpio(0);
 var port = process.env.PORT || 5000;
 
 myLed.dir(m.DIR_OUT);
 
 app.get('/', function(req, res) { console.dir(req)
 
-    res.send('Led is ' + (myLed.read()?'off.':'on.'));
+    res.send('<h1>Light is ' + (myLed.read()?'off.':'on.') + '</h1>');
 
     logText = new Date().toJSON() + ": Info request.\n"
     fs.appendFile('myLinkIt.log', logText, function(err) { if (err) throw err; });
@@ -26,7 +26,7 @@ app.get('/', function(req, res) { console.dir(req)
 app.get('/on', function(req, res) {
 
     myLed.write(0);                                   
-    res.send('Led is ' + (myLed.read()?'off.':'on.'));
+    res.send('<h1>Light is ' + (myLed.read()?'off.':'on.') + '</h1>');
 
     logText = new Date().toJSON() + ": ON request.\n"
     fs.appendFile('myLinkIt.log', logText, function(err) { if (err) throw err; });
@@ -40,7 +40,7 @@ app.get('/on', function(req, res) {
 app.get('/off', function(req, res) {
 
     myLed.write(1);                                   
-    res.send('Led is ' + (myLed.read()?'off.':'on.'));
+    res.send('<h1>Light is ' + (myLed.read()?'off.':'on.') + '</h1>');
 
     logText = new Date().toJSON() + ": OFF request.\n"
     fs.appendFile('myLinkIt.log', logText, function(err) { if (err) throw err; });
